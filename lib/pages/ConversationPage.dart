6f2fe617eab6bc05cf0041286d5ebc716538f064
Michael Helfer <mike@mikehelfer.com>
2019-10-07 22:39:07 -0500

    in progress day 2

---
+++ lib/pages/ConversationPage.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart'
+import 'package:messio/widgets/ChatAppBar.dart';
+import 'package:messio/widgets/ChatListWidget.dart';
+import 'package:messio/widgets/InputWidget.dart';
