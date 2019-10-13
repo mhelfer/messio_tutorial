9b9258bd4ce7db54033a0ee1a6638495e18d89d8
Michael Helfer <mike@mikehelfer.com>
2019-10-13 14:41:49 -0500

    removing broken hello world test that will be replaced by more relevant tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,9 +10,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/main.dart';
 
 void main() {
-  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(Messio());
-    expect(find.text('Hello World!'), findsOneWidget);
-  });
+
 }
