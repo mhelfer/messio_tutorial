c213d70ef6523943f26b3c8d7a37b6e3310faec7
Michael Helfer <mike@mikehelfer.com>
2019-10-13 20:45:40 -0500

    added UI Widget Tests per 'Day 4–5 : Widget Testing With Flutter'

---
+++ test/widgets/ChatAppBar_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:messio/widgets/ChatAppBar.dart';
+
+void main(){
+  const MaterialApp app = MaterialApp(
+    home: Scaffold(
+      body: const ChatAppBar(),
+    ),
+  );
+
+  testWidgets("ChatAppBar UI Test", (WidgetTester tester) async {
+    await tester.pumpWidget(app);
+
+    expect(find.text('Michael Helfer'), findsOneWidget);
+    expect(find.text('@mikehelfer'), findsOneWidget);
+    expect(find.byType(IconButton),findsNWidgets(1));
+    expect(find.byType(CircleAvatar),findsOneWidget);
+  });
+}
